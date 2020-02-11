e67a93020635ed86dff0f0ebace4184ce52c3c49
Ernesto <ernestoangulo_15@outlook.com>
2020-02-11 18:30:27 -0500

    JS register bug fixed

--- js/register.js
+++ js/register.js
@@ -108,26 +108,28 @@
             console.log(chosenDays);
         }
 
-        let validateFields = () => {
-            if (this.value === '') {
-                errorDiv.style.display = 'block';
-                errorDiv.innerHTML = 'Todos los campos son obligatorios';
-                this.style.border = '1px solid red';
-                errorDiv.style.border = '1px solid red'
-            } else {
-                errorDiv.style.display = 'none';
-                this.style.border = '1px solid #cccccc';
-            }
+        let validateFields = (e) => {
+          self = e.target
+          if (self.value === '') {
+            errorDiv.style.display = 'block';
+            errorDiv.innerHTML = 'Todos los campos son obligatorios';
+            self.style.border = '1px solid red';
+            errorDiv.style.border = '1px solid red'
+          } else {
+            errorDiv.style.display = 'none';
+            self.style.border = '1px solid #cccccc';
+          }
         }
 
-        let validateEmail = () => {
-            if (this.value.indexOf('@') > -1) {
+        let validateEmail = (e) => {
+          self = e.target
+            if (self.value.indexOf('@') > -1) {
                 errorDiv.style.display = 'none';
-                this.style.border = '1px solid #cccccc';
+                self.style.border = '1px solid #cccccc';
             } else {
                 errorDiv.style.display = 'block';
                 errorDiv.innerHTML = 'Ingrese E-mail valido';
-                this.style.border = '1px solid red';
+                self.style.border = '1px solid red';
                 errorDiv.style.border = '1px solid red'
             }
         }
